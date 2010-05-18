61058cbd88b1adcb4209ff8e98e02b4a141a0957
Jonathan Rudenberg <jonathan@titanous.com>
2010-05-18 09:55:03 -0400

    Add missing non-matching specs for conditions

--- spec/mailman/route/conditions_spec.rb
+++ spec/mailman/route/conditions_spec.rb
@@ -6,6 +6,10 @@ describe Mailman::Route::ToCondition do
     Mailman::Route::ToCondition.new('test').match(basic_message).should == [{}, []]
   end
 
+  it 'should not match a non-matching address' do
+    Mailman::Route::ToCondition.new('foo').match(basic_message).should be_nil
+  end
+
   it 'should define a method on Route that is chainable and stores the condition' do
     Mailman::Route.new.to('test').conditions[0].class.should == Mailman::Route::ToCondition
   end
@@ -16,6 +20,10 @@ describe Mailman::Route::FromCondition do
 
   it 'should match an address' do
     Mailman::Route::FromCondition.new('chunky').match(basic_message).should == [{}, []]
+  end
+
+  it 'should not match a non-matching address' do
+    Mailman::Route::FromCondition.new('foo').match(basic_message).should be_nil
   end
 
   it 'should define a method on Route that is chainable and stores the condition' do
@@ -30,6 +38,10 @@ describe Mailman::Route::SubjectCondition do
     Mailman::Route::SubjectCondition.new('Hello').match(basic_message).should == [{}, []]
   end
 
+  it 'should not match a non-matching subject' do
+    Mailman::Route::SubjectCondition.new('foo').match(basic_message).should be_nil
+  end
+
   it 'should define a method on Route that is chainable and stores the condition' do
     Mailman::Route.new.subject('test').conditions[0].class.should == Mailman::Route::SubjectCondition
   end
@@ -40,6 +52,10 @@ describe Mailman::Route::BodyCondition do
 
   it 'should match the body' do
     Mailman::Route::BodyCondition.new('email').match(basic_message).should == [{}, []]
+  end
+
+  it 'should not match a non-matching body' do
+    Mailman::Route::BodyCondition.new('foo').match(basic_message).should be_nil
   end
 
   it 'should define a method on Route that is chainable and stores the condition' do
