fb0c89410c4daeaa0630fef02bf348a2ba22f513
Jonathan Rudenberg <jonathan@titanous.com>
2010-06-10 17:19:56 -0400

    more router specs

--- spec/mailman/router_spec.rb
+++ spec/mailman/router_spec.rb
@@ -14,18 +14,41 @@ describe Mailman::Router do
   describe 'routing' do
 
     before do 
-      @route = TestRoute.new
-      @router.add_route(@route)
+      @route1 = TestRoute.new
+      @route1.correct_message = 'test1'
+      @router.add_route(@route1)
     end
 
     it 'should work without block args' do
-      @route.block = lambda { params[:test] == 'test' } 
-      @router.route('test').should be_true
+      @route1.block = lambda { params[:test] == 'test' } 
+      @router.route('test1').should be_true
     end
 
     it 'should work with block args' do
-      @route.block = lambda { |arg1,arg2| arg1 == 'test' and arg2 == 'testing' and params[:test] == 'test' } 
-      @router.route('test').should be_true
+      @route1.block = lambda { |arg1,arg2| arg1 == 'test' and arg2 == 'testing' and params[:test] == 'test' } 
+      @router.route('test1').should be_true
+    end
+
+    describe 'list' do
+
+      before do
+        @route2 = TestRoute.new
+        @router.add_route(@route2)
+      end
+
+      it 'should loop through routes and find the first route that matches' do
+        @route2.block = lambda { 2 }
+        @route2.correct_message = 'test2'
+        @router.route('test2').should == 2
+      end
+
+      it 'should run the first route that matches with two matching routes' do
+        @route2.correct_message = 'test1'
+        @route1.block = lambda { 1 }
+        @route2.block = lambda { 2 }
+        @router.route('test1').should == 1
+      end
+
     end
 
   end
@@ -34,9 +57,10 @@ end
 
 class TestRoute
 
-  attr_accessor :block
+  attr_accessor :block, :correct_message
 
   def match!(message)
-    { :block => @block, :params => {:test => 'test'}, :args => ['test', 'testing'] } if message == 'test'
+    { :block => @block, :params => {:test => 'test'}, :args => ['test', 'testing'] } if message == @correct_message
   end
+
 end
