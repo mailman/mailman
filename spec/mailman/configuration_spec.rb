665d3abdb996027a8a09a03a036c5ff1cf296c69
Jonathan Rudenberg <jonathan@titanous.com>
2010-06-17 13:27:01 -0400

    Fix logger config spec properly

--- spec/mailman/configuration_spec.rb
+++ spec/mailman/configuration_spec.rb
@@ -2,18 +2,28 @@ require File.expand_path(File.join(File.dirname(__FILE__), '..', '/spec_helper')
 
 describe Mailman::Configuration do
 
-   # in spec_helper config = Mailman::Configuration
+  # in spec_helper config = Mailman::Configuration
 
-  it 'should have a default logger' do
-    config.logger = nil
-    config.logger.instance_variable_get('@logdev').dev.should == STDOUT
-  end
+  describe 'logger' do
 
-  it 'should store a custom logger using the class method' do
-    original_logger = config.logger
-    config.logger = Logger.new(STDERR)
-    config.logger.instance_variable_get('@logdev').dev.should == STDERR
-    config.logger = original_logger
+    before do
+      @original_logger = config.logger
+    end
+
+    after do
+      config.logger = @original_logger
+    end
+
+    it 'should have a default logger' do
+      config.logger = nil
+      config.logger.instance_variable_get('@logdev').dev.should == STDOUT
+    end
+
+    it 'should store a custom logger using the class method' do
+      config.logger = Logger.new(STDERR)
+      config.logger.instance_variable_get('@logdev').dev.should == STDERR
+    end
+
   end
 
 end
