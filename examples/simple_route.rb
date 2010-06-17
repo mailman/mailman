962b4c3d59f91b82c59456643aaa6bb1369a8764
Jonathan Rudenberg <jonathan@titanous.com>
2010-06-17 11:13:32 -0400

    Add basic example

---
+++ examples/simple_route.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'mailman'
+
+Mailman::Application.new do
+  from('%user%@%domain%') do
+    puts "Got #{message.subject} from #{params[:user]}"
+  end
+end.run
+
+# cat ../spec/fixtures/example01.eml | ./simple_route.rb
