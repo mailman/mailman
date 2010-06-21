49cd22d7f283030a79ddb7e2aaf00199c8887faf
Jonathan Rudenberg <jonathan@titanous.com>
2010-06-21 10:55:40 -0400

    POP3 config and poll interval

--- lib/mailman/configuration.rb
+++ lib/mailman/configuration.rb
@@ -6,8 +6,19 @@ module Mailman
       # @return [Logger] the application's logger
       attr_accessor :logger
 
+      # @return [Hash] the configuration hash for POP3
+      attr_accessor :pop3
+
+      # @return [Fixnum] the poll interval for POP3 or IMAP. Setting this to 0
+      #   disables polling
+      attr_accessor :poll_interval
+
       def logger
         @logger ||= Logger.new(STDOUT)
+      end
+
+      def poll_interval
+        @poll_interval ||= 60
       end
 
     end
--- spec/mailman/configuration_spec.rb
+++ spec/mailman/configuration_spec.rb
@@ -19,11 +19,28 @@ describe Mailman::Configuration do
       config.logger.instance_variable_get('@logdev').dev.should == STDOUT
     end
 
-    it 'should store a custom logger using the class method' do
+    it 'should store a custom logger' do
       config.logger = Logger.new(STDERR)
       config.logger.instance_variable_get('@logdev').dev.should == STDERR
     end
 
+  end
+
+  it 'should store the POP3 config hash' do
+    config.pop3 = {:user => 'foo'}
+    config.pop3.should == {:user => 'foo'}
+    config.pop3 = nil
+  end
+
+  it 'should have a default poll interval' do
+    config.poll_interval = nil
+    config.poll_interval.should == 60
+  end
+
+  it 'should store the poll interval' do
+    config.poll_interval = 20
+    config.poll_interval.should == 20
+    config.poll_interval = nil
   end
 
 end
