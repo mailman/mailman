3e19d9b29921d2303fc4640db3b1818fe5fae1bd
wvengen <dev-rails@willem.engen.nl>
2014-12-21 22:19:23 +0100

    Improve passing options to pop3 and imap backends

--- lib/mailman/receiver/pop3.rb
+++ lib/mailman/receiver/pop3.rb
@@ -12,16 +12,21 @@ module Mailman
       # @option options [MessageProcessor] :processor the processor to pass new
       #   messages to
       # @option options [String] :server the server to connect to
-      # @option options [Integer] :port the port to connect to
+      # @option options [Integer] :port the port to connect to (default 110, or 995 for ssl)
       # @option options [String] :username the username to authenticate with
       # @option options [String] :password the password to authenticate with
-      # @option options [true,false] :ssl enable SSL
+      # @option options [true,false,Hash] :ssl enable SSL
       def initialize(options)
         @processor = options[:processor]
         @username = options[:username]
         @password = options[:password]
-        @connection = Net::POP3.new(options[:server], options[:port])
-        @connection.enable_ssl if options[:ssl]
+        port = options[:port] || (options[:ssl] ? 995 : 110)
+        @connection = Net::POP3.new(options[:server], port)
+        if options[:ssl].is_a? Hash
+          @connection.enable_ssl(options[:ssl])
+        elsif options[:ssl]
+          @connection.enable_ssl
+        end
         @connection.open_timeout = options[:open_timeout] if options[:open_timeout]
         @connection.read_timeout = options[:read_timeout] if options[:read_timeout]
       end
